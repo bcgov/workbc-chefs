bb91755b3816f63683a7953bad2ef7e6fe0bff1f
Branko Bajic <eisoftware@mailfence.com>
2026-08-24 16:22:07 -0700

    remove email logic (temp)

--- app/src/forms/submission/service.js
+++ app/src/forms/submission/service.js
@@ -136,7 +136,7 @@ const service = {
             await service.changeStatusState(formSubmissionId, { code: Statuses.SUBMITTED }, currentUser, trx);
             // If finalizing submission, send the submission email (quiet fail if anything goes wrong)
             const submissionMetaData = await SubmissionMetadata.query().where('submissionId', formSubmissionId).first();
-            emailService.submissionReceived(submissionMetaData.formId, formSubmissionId, data, referrer).catch(() => {});
+            //emailService.submissionReceived(submissionMetaData.formId, formSubmissionId, data, referrer).catch(() => {}); // TODO: fix formService bug
           }
         } else {
           if (statuses && statuses.length > 0 && (statuses[0].code === Statuses.SUBMITTED || statuses[0].code === Statuses.COMPLETED)) {
