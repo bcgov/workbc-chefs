446146c116d4c37c8be836941aa52abf97d68594
Branko Bajic <eisoftware@mailfence.com>
2026-09-22 18:44:34 -0700

    console log logic

--- app/src/forms/submission/service.js
+++ app/src/forms/submission/service.js
@@ -164,7 +164,9 @@ const service = {
       const JCPFormID = config.get('serviceClient.oes.cfms.JCPFormId');
       const RIFormID = config.get('serviceClient.oes.cfms.RIFormId');
       const formID = formVersion.formId;
-      console.log('[submission service - CEP] SubmissionID: ', formSubmissionId, ' & formVersionId: ', formVersionId, ' & formId: ', formID);
+      if (!data.draft) {
+        console.log('[submission service - CEP] SubmissionID: ', formSubmissionId, ' & formVersionId: ', formVersionId, ' & formId: ', formID);
+      }
       if (!data.draft && (formID == PBLMTFormID || formID == LMPFormID || formID == JCPFormID || formID == RIFormID)) {
         console.log('[submission service - CEP] ===== CFMS Logic =====');
         try {
